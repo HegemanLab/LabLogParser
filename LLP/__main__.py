272c4a5172e5c83a018af24ba4504f4697eccc00
Luke Gentle <gentl017@morris.umn.edu>
2019-08-15 10:50:24 -0500

    Updated main and removed extra copy of main

--- LLP/__main__.py
+++ LLP/__main__.py
@@ -1,10 +1,5 @@
 #The functions for opening files and reading configs files
 import LLP.read
-#The functions for writing to the influxDB database
-import LLP.write
-from os import listdir
-#Used to check if a file exists
-from os.path import isfile, join
 #The library used for determining if a file exists
 import os.path
 #Library for passing arguments with python script
@@ -31,8 +26,8 @@ def main():
 	else:																#Parse the file provided.
 		n = 1
 		while(n < len(sys.argv)):										#For each config file passed
-			configs = LLP.read.parseConfigFile(sys.argv[n])						#Parse the config file
-			LLP.read.fileSelector(configs)										#Run the parser based on the config file's settings
+			configs = LLP.read.parseConfigFile(sys.argv[n])				#Parse the config file
+			LLP.read.fileSelector(configs)								#Run the parser based on the config file's settings
 			n += 1
 
 
